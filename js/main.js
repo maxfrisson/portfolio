644d0ae54f1fec5efa991897e2512dc14a1d37dd
Max Frisson <max.chebonenko@gmail.com>
2021-02-13 18:12:54 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function toggle() {
+  var sec = document.getElementById('sec');
+  var nav = document.getElementById('navigation');
+  sec.classList.toggle('active');
+  nav.classList.toggle('active');
+}
+
